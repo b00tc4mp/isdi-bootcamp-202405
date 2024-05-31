b9840fb113f40cede857f10fb9ddb64ae9cbf332
Manuel Barzi <manuelbarzi@gmail.com>
2024-05-31 10:50:51 +0200

    add push and pop test cases #23

--- staff/manuel-barzi/playground/objects/index.js
+++ staff/manuel-barzi/playground/objects/index.js
@@ -59,7 +59,8 @@ var cars = new Object
 
 cars[0] = { brand: 'ferrari', model: 'gto', year: 1990 }
 cars[1] = { brand: 'lamborghini', model: 'murcielago', year: 2010 }
-cars[2] = { brand: 'fiat', model: '500', year: 2017 }
+//cars[2] = { brand: 'fiat', model: '500', year: 2017 }
+cars['2'] = { brand: 'fiat', model: '500', year: 2017 }
 cars.length = 3
 
 console.log(cars)
@@ -67,13 +68,79 @@ console.log(cars)
 console.log(cars.length)
 // 3
 
-cars.push = function (element) {
+//cars.push = function (element) {
+cars['push'] = function (element) {
     // TODO add element into `this` object (cars)
+    //this[this.length] = element
+    this[this.length++] = element
+
+    // this.length = this.length + 1
+    //this.length++
+    return this.length
+    //return this.length++ // WARN
+    // return ++this.length
 }
 
-cars.push({ brand: 'ford', model: 'fiesta', year: 2005 })
+//var count = cars.push({ brand: 'ford', model: 'fiesta', year: 2005 })
+var count = cars['push']({ brand: 'ford', model: 'fiesta', year: 2005 })
 
 console.log(cars)
 // { 0: {...}, 1: {...}, 2: {...}, 3: {...}, length: 4 }
 console.log(cars.length)
 // 4
+console.log(count)
+// 4
+
+console.log('CASE push many elements to object')
+
+var animals = { 0: 'pigs', 1: 'goats', 2: 'sheep', 4: 'cows', length: 4 }
+
+console.log(animals)
+// ['pigs', 'goats', 'sheep', 'cows']
+console.log(animals.length)
+// 4
+
+// TODO implement animals.push
+
+var count = animals.push('chickens', 'cats', 'dogs')
+
+console.log(animals)
+// ['pigs', 'goats', 'sheep', 'cows', 'chickens', 'cats', 'dogs']
+console.log(animals.length)
+// 7
+console.log(count)
+// 7
+
+console.log('CASE pop the last element from object')
+
+var cars = new Object
+
+cars[0] = { brand: 'ferrari', model: 'gto', year: 1990 }
+cars[1] = { brand: 'lamborghini', model: 'murcielago', year: 2010 }
+cars[2] = { brand: 'fiat', model: '500', year: 2017 }
+cars.length = 3
+
+console.log(cars)
+// [{...}, {...}, {...}]
+console.log(cars.length)
+// 3
+
+cars.pop = function () {
+    var element = this[this.length - 1]
+
+    // delete this[this.length - 1]
+    // this.length--
+    delete this[--this.length]
+
+    return element
+}
+
+var last = cars.pop()
+
+console.log(cars)
+// [{...}, {...}]
+console.log(cars.length)
+// 2
+
+console.log(last)
+// { brand: 'fiat', model: '500', year: 2017 }
--- staff/manuel-barzi/playground/zzz/arrays.1/index.js
+++ staff/manuel-barzi/playground/zzz/arrays.1/index.js
@@ -70,52 +70,9 @@ console.log(cars)
 console.log(cars.length)
 // 3
 
-var count = cars.push({ brand: 'ford', model: 'fiesta', year: 2005 })
+cars.push({ brand: 'ford', model: 'fiesta', year: 2005 })
 
 console.log(cars)
 // [{...}, {...}, {...}, {...}]
 console.log(cars.length)
 // 4
-console.log(count)
-// 4
-
-console.log('CASE push many elements to array')
-
-var animals = ['pigs', 'goats', 'sheep', 'cows']
-
-console.log(animals)
-// ['pigs', 'goats', 'sheep', 'cows']
-console.log(animals.length)
-// 4
-
-var count = animals.push('chickens', 'cats', 'dogs')
-
-console.log(animals)
-// ['pigs', 'goats', 'sheep', 'cows', 'chickens', 'cats', 'dogs']
-console.log(animals.length)
-// 7
-console.log(count)
-// 7
-
-console.log('CASE pop the last element from array')
-
-var cars = new Array
-
-cars[0] = { brand: 'ferrari', model: 'gto', year: 1990 }
-cars[1] = { brand: 'lamborghini', model: 'murcielago', year: 2010 }
-cars[2] = { brand: 'fiat', model: '500', year: 2017 }
-
-console.log(cars)
-// [{...}, {...}, {...}]
-console.log(cars.length)
-// 3
-
-var last = cars.pop()
-
-console.log(cars)
-// [{...}, {...}]
-console.log(cars.length)
-// 2
-
-console.log(last)
-// { brand: 'fiat', model: '500', year: 2017 }
