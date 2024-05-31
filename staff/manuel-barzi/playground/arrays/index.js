4144e52cff1338169823c63f1ea0fbfd65f26357
Manuel Barzi <manuelbarzi@gmail.com>
2024-05-31 13:04:07 +0200

    add more test cases for arrays and objects #23

--- staff/manuel-barzi/playground/arrays/index.js
+++ staff/manuel-barzi/playground/arrays/index.js
@@ -79,7 +79,7 @@ console.log(cars.length)
 console.log(count)
 // 4
 
-console.log('CASE push many elements to array')
+console.log('CASE push multiple elements to array')
 
 var animals = ['pigs', 'goats', 'sheep', 'cows']
 
@@ -119,3 +119,89 @@ console.log(cars.length)
 
 console.log(last)
 // { brand: 'fiat', model: '500', year: 2017 }
+
+console.log('CASE element at index')
+
+var nums = [5, 12, 8, 130, 44]
+
+console.log(nums)
+// [5, 12, 8, 130, 44]
+console.log(nums.length)
+// 5
+
+var num = nums.at(3)
+
+console.log(num)
+// 130
+
+var num = nums.at(0)
+
+console.log(num)
+// 5
+
+var num = nums.at(-3)
+
+console.log(num)
+// 8
+
+var num = nums.at(100)
+
+console.log(num)
+// undefined
+
+var num = nums.at(-100)
+
+console.log(num)
+// undefined
+
+console.log('CASE concat elements from two arrays')
+
+var chars1 = ['a', 'b', 'c']
+var chars2 = ['d', 'e', 'f']
+
+console.log(chars1)
+// ['a', 'b', 'c']
+console.log(chars2)
+// ['d', 'e', 'f']
+
+var chars3 = chars1.concat(chars2)
+
+console.log(chars1)
+// ['a', 'b', 'c']
+console.log(chars2)
+// ['d', 'e', 'f']
+console.log(chars3)
+// ['a', 'b', 'c', 'd', 'e', 'f']
+
+console.log('CASE concat elements from 5 arrays')
+
+var nums1 = [10, 20, 30]
+var nums2 = [400, 500]
+var nums3 = [-60, -70]
+var nums4 = [800, 900]
+var nums5 = [-1000]
+
+var nums6 = nums1.concat(nums2, nums3, nums4, nums5)
+
+console.log(nums1)
+// [10, 20, 30]
+console.log(nums2)
+// [400, 500]
+console.log(nums3)
+// [-60, -70]
+console.log(nums4)
+// [800, 900]
+console.log(nums5)
+// [-1000]
+
+console.log(nums6)
+// [10, 20, 30, 400, 500, -60, -70, 800, 900, -1000]
+
+// TODO implement case for join
+// TODO implement case for includes
+// TODO implement case for indexOf
+// TODO implement case for lastIndexOf
+// TODO implement case for slice
+// TODO implement case for reverse
+// TODO implement case for shift
+// TODO implement case for copyWithin
--- staff/manuel-barzi/playground/objects/index.js
+++ staff/manuel-barzi/playground/objects/index.js
@@ -91,21 +91,37 @@ console.log(cars.length)
 console.log(count)
 // 4
 
-console.log('CASE push many elements to object')
+console.log('CASE push multiple elements to object')
 
-var animals = { 0: 'pigs', 1: 'goats', 2: 'sheep', 4: 'cows', length: 4 }
+var animals = { 0: 'pigs', 1: 'goats', 2: 'sheep', 3: 'cows', length: 4 }
 
 console.log(animals)
 // ['pigs', 'goats', 'sheep', 'cows']
 console.log(animals.length)
 // 4
 
-// TODO implement animals.push
+// animals.push = function (element1, element2, element3) {
+//     this[this.length++] = element1
+//     this[this.length++] = element2
+//     this[this.length++] = element3
+
+//     return this.length
+// }
+
+animals.push = function () {
+    for (var index = 0; index < arguments.length; index++) {
+        var argument = arguments[index]
+
+        this[this.length++] = argument
+    }
+
+    return this.length
+}
 
 var count = animals.push('chickens', 'cats', 'dogs')
 
 console.log(animals)
-// ['pigs', 'goats', 'sheep', 'cows', 'chickens', 'cats', 'dogs']
+// { 0: 'pigs', 1: 'goats', 2: 'sheep', 3: 'cows', 4: 'chickens', 5: 'cats', 6: 'dogs', length: 7 } 
 console.log(animals.length)
 // 7
 console.log(count)
@@ -121,7 +137,7 @@ cars[2] = { brand: 'fiat', model: '500', year: 2017 }
 cars.length = 3
 
 console.log(cars)
-// [{...}, {...}, {...}]
+// { 0: {...}, 1: {...}, 2: {...}, length: 3 }
 console.log(cars.length)
 // 3
 
@@ -144,3 +160,110 @@ console.log(cars.length)
 
 console.log(last)
 // { brand: 'fiat', model: '500', year: 2017 }
+
+console.log('CASE element at index')
+
+var nums = { 0: 5, 1: 12, 2: 8, 3: 130, 4: 44, length: 5 }
+
+console.log(nums)
+// [5, 12, 8, 130, 44]
+console.log(nums.length)
+// 5
+
+nums.at = function (index) {
+    //if (index >= 0)
+    if (index > -1)
+        return this[index]
+    else
+        return this[this.length + index]
+}
+
+var num = nums.at(3)
+
+console.log(num)
+// 130
+
+var num = nums.at(0)
+
+console.log(num)
+// 5
+
+var num = nums.at(-3)
+
+console.log(num)
+// 8
+
+var num = nums.at(100)
+
+console.log(num)
+// undefined
+
+var num = nums.at(-100)
+
+console.log(num)
+// undefined
+
+console.log('CASE concat elements from two objects')
+
+var chars1 = { 0: 'a', 1: 'b', 2: 'c', length: 3 }
+var chars2 = { 0: 'd', 1: 'e', 2: 'f', length: 3 }
+
+console.log(chars1)
+// { 0: 'a', 1: 'b', 2: 'c', length: 3 }
+console.log(chars2)
+// { 0: 'd', 1: 'e', 2: 'f', length: 3 }
+
+chars1.concat = function (object) {
+    var newObject = { length: 0 }
+
+    for (var i = 0; i < this.length; i++) {
+        var elem = this[i]
+
+        newObject[newObject.length++] = elem
+    }
+
+    for (var i = 0; i < object.length; i++) {
+        var elem = object[i]
+
+        newObject[newObject.length++] = elem
+    }
+
+    return newObject
+}
+
+var chars3 = chars1.concat(chars2)
+
+console.log(chars1)
+// { 0: 'a', 1: 'b', 2: 'c', length: 3 }
+console.log(chars2)
+// { 0: 'd', 1: 'e', 2: 'f', length: 3 }
+console.log(chars3)
+// { 0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', length: 6 }
+
+console.log('CASE concat elements from 5 objects')
+
+var nums1 = { 0: 10, 1: 20, 2: 30, length: 3 }
+var nums2 = { 0: 400, 1: 500, length: 2 }
+var nums3 = { 0: -60, 1: -70, length: 2 }
+var nums4 = { 0: 800, 1: 900, length: 2 }
+var nums5 = { 0: -1000, length: 1 }
+
+nums1.concat = function () {
+    // TODO implement me (USE this, arguments)
+}
+
+var nums6 = nums1.concat(nums2, nums3, nums4, nums5)
+
+console.log(nums1)
+// { 0: 10, 1: 20, 2: 30, length: 3 }
+console.log(nums2)
+// { 0: 400, 1: 500, length: 2 }
+console.log(nums3)
+// { 0: -60, 1: -70, length: 2 }
+console.log(nums4)
+// { 0: 800, 1: 900, length: 2 }
+console.log(nums5)
+// { 0: -1000, length: 1 }
+
+console.log(nums6)
+// { 0: 10, 1: 20, 2: 30, 3: 400, 4: 500, 5: -60, 6: -70, 7: 800, 8: 900, 9: -1000, length: 10 }
