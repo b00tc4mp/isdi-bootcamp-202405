15c9937dd01f76b7275536f935c78c6788ca03f6
Manuel Barzi <manuelbarzi@gmail.com>
2024-06-05 13:25:09 +0200

    simplify game into objects; add Actor constructor function and apply it; #23

--- staff/manuel-barzi/playground/zzz/game.6/main.js
+++ staff/manuel-barzi/playground/zzz/game.6/main.js
@@ -1,3 +1,44 @@
+function Actor(container, width, height) {
+    this.container = container
+
+    this.setX(0)
+    this.setY(0)
+
+    this.w = width
+    this.h = height
+
+    this.container.style.width = this.w + 'px'
+    this.container.style.height = this.h + 'px'
+}
+
+Actor.prototype.getXMax = function () { return this.x + this.w }
+
+Actor.prototype.getYMax = function () { return this.y + this.h }
+
+Actor.prototype.setX = function (x) {
+    this.x = x
+
+    this.container.style.left = this.x + 'px'
+}
+
+Actor.prototype.setY = function (y) {
+    this.y = y
+
+    this.container.style.top = this.y + 'px'
+}
+
+Actor.prototype.moveX = function (dx) {
+    this.setX(this.x + dx)
+}
+
+Actor.prototype.moveY = function (dy) {
+    this.setY(this.y + dy)
+}
+
+Actor.prototype.setColor = function (color) {
+    this.container.style.backgroundColor = color
+}
+
 var actor1 = new Actor(document.getElementById('box-1'), 40, 80)
 var actor2 = new Actor(document.getElementById('box-2'), 70, 40)
 
