91cfcf5aed57d05c3f57f668a9cc4cce92a17a94
Tatiana_Garcia <151873298+TatiGV@users.noreply.github.com>
2024-06-05 20:01:06 +0200

    forEach method and map method of arrays and forEach method of modified objects #32

--- staff/tatiana-garcia/playground/arrays/map.js
+++ staff/tatiana-garcia/playground/arrays/map.js
@@ -4,7 +4,7 @@ console.log('CASE map from arrays')
 
 var nums = [1, 4, 9, 16];
 
-var nums = array1.map((x) => x * 2);
+var map1 = array1.map((x) => x * 2);
 
 console.log(map1);
 // Expected output: Array [2, 8, 18, 32]
