e5be041eb9c1008303e7d7ea2cfc30577695cc46
Tatiana_Garcia <151873298+TatiGV@users.noreply.github.com>
2024-06-05 20:07:48 +0200

    modifiedmodified game, actor implementation #32

--- staff/tatiana-garcia/playground/game/main.js
+++ staff/tatiana-garcia/playground/game/main.js
@@ -1,76 +1,38 @@
-var box1 = document.getElementById('box-1')
-var box2 = document.getElementById('box-2')
+var actor1 = new Actor(document.getElementById('box-1'), 40, 80)
+var actor2 = new Actor(document.getElementById('box-2'), 70, 40)
 
-box1.style.backgroundColor = 'red'
-box2.style.backgroundColor = 'dodgerblue'
+actor1.setColor('red')
+actor2.setColor('dodgerblue')
 
-var x1 = 100, y1 = 100, w1 = 50, h1 = 100, x2 = 300, y2 = 300, w2 = 70, h2 = 40, STEP = 10
+actor1.setX(120)
+actor1.setY(100)
 
-box1.style.left = x1 + 'px'
-box1.style.top = y1 + 'px'
-box1.style.width = w1 + 'px'
-box1.style.height = h1 + 'px'
+actor2.setX(200)
+actor2.setY(200)
 
-box2.style.left = x2 + 'px'
-box2.style.top = y2 + 'px'
-box2.style.width = w2 + 'px'
-box2.style.height = h2 + 'px'
+var STEP = 10
 
 document.onkeydown = function (event) {
-    // console.log(event.key) //cuando presiones la tecla salga la tecla que tocamos
+    // console.log(event.key)
 
-    if (event.key === 'ArrowRight') {
+    if (event.key === 'ArrowRight')
+        actor1.moveX(STEP)
+    else if (event.key === 'ArrowLeft')
+        actor1.moveX(-STEP)
+    else if (event.key === 'ArrowDown')
+        actor1.moveY(STEP)
+    else if (event.key === 'ArrowUp')
+        actor1.moveY(-STEP)
 
-        x1 += STEP
-    } else if (event.key === 'ArrowLeft') {
+    if (event.key === 'd')
+        actor2.moveX(STEP)
+    else if (event.key === 'a')
+        actor2.moveX(-STEP)
+    else if (event.key === 's')
+        actor2.moveY(STEP)
+    else if (event.key === 'w')
+        actor2.moveY(-STEP)
 
-        x1 -= STEP
-
-    } else if (event.key === 'ArrowDown') {
-
-        y1 += STEP
-
-    } else if (event.key === 'ArrowUp') {
-
-        y1 -= STEP
-
-    }
-
-    box1.style.left = x1 + 'px'
-    box1.style.top = y1 + 'px'
-
-    if (event.key === 'd') {
-
-        x2 += STEP
-    } else if (event.key === 'a') {
-
-        x2 -= STEP
-
-    } else if (event.key === 's') {
-
-        y2 += STEP
-
-    } else if (event.key === 'w') {
-
-        y2 -= STEP
-
-    }
-
-
-    box2.style.left = x2 + 'px'
-    box2.style.top = y2 + 'px'
-
-    // TODO improve intersection detection
-
-    if (x1 < x2 + w2 &&
-        x1 + w1 > x2 &&
-        y1 < y2 + h2 &&
-        y1 + h1 > y2) {
-
-        console.log('intersecting')
-
-    }
-
-
-}
-
+    if (actor1.x <= actor2.getXMax() && actor1.y <= actor2.getYMax() && actor1.getXMax() >= actor2.x && actor1.getYMax() >= actor2.y)
+        console.count('intersecting')
+};
