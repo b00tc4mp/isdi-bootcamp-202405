85a7be3df9fdb0007bddeb8f0ce83c3932d64e87
Matias Sargo <matiassargo@outlook.com>
2024-06-10 14:51:06 +0200

    add map prototype #31

--- staff/matias-sargo/playground/Array/Array.prototype.map.test.js
+++ staff/matias-sargo/playground/Array/Array.prototype.map.test.js
@@ -12,3 +12,17 @@ console.assert(numsBy2[1] === 8, 'map1 at 1 is 8')
 console.assert(numsBy2[2] === 18, 'map1 at 2 is 18')
 console.assert(numsBy2[3] === 32, 'map1 at 3 is 32')
 console.assert(numsBy2.length === nums.length, 'map1 length is 4')
+
+console.info("CASE maps cart items to string with stats")
+
+var cart = [
+    { brand: "adidas", name: "cool socks", price: 10, quantity: 2 },
+    { brand: "nike", name: "cool air", price: 200, quantity: 1 },
+    { brand: "armani", name: "cool glasses", price: 250, quantity: 1 },
+    { brand: "calvin klein", name: "cool boxers", price: 30, quantity: 3 }
+]
+
+var stats = cart.map(function (element, index, items) {
+    var total = 0
+
+})
