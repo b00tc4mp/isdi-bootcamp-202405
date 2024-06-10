a0c181ab2441b6cfb3ef79abdcab801c4118eea2
Matias Sargo <matiassargo@outlook.com>
2024-06-10 14:52:56 +0200

    add map prototype #31

--- staff/matias-sargo/playground/Curray/Curray.prototype.map.test.js
+++ staff/matias-sargo/playground/Curray/Curray.prototype.map.test.js
@@ -16,3 +16,6 @@ console.assert(numsBy2[1] === 8, 'map1 at 1 is 8')
 console.assert(numsBy2[2] === 18, 'map1 at 2 is 18')
 console.assert(numsBy2[3] === 32, 'map1 at 3 is 32')
 console.assert(numsBy2.length === nums.length, 'map1 length is 4')
+
+console.info("CASE maps cart items to string with stats")
+
