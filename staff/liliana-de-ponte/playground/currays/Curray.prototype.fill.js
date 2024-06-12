cf3aa86bf76c02f0ad2d71cec303154050b4257f
Liliana De Ponte <lilideponte@gmail.com>
2024-06-12 11:34:26 +0200

    implement fill method in Array and Curray, #24

---
+++ staff/liliana-de-ponte/playground/currays/Curray.prototype.fill.js
@@ -0,0 +1,26 @@
+var Curray = require('./Curray')
+
+Curray.prototype.fill = function (value, start, end) {
+    if (start === undefined || start < -this.length) {
+        start = 0
+    }
+    else if (start > this.length - 1) {
+        return this
+    }
+    else if (start < 0) {
+        start = this.length + start
+    }
+    if (end < 0) {
+        end = this.length + end
+    }
+    else if (end === undefined || end > this.length - 1) {
+        end = this.length
+    }
+    else if (end <= start) {
+        return this
+    }
+    for (var i = start; i < end; i++) {
+        this[i] = value
+    }
+    return this
+}
---
+++ staff/liliana-de-ponte/playground/currays/Curray.prototype.fill.test.js
@@ -0,0 +1,34 @@
+var Curray = require('./Curray')
+require('./Curray.prototype.fill')
+
+console.info('TEST Curray.prototype.fill')
+
+console.info('CASE fill with 1 parameter in Curray')
+var numbers = new Curray(1, 2, 3, 4)
+var numbers1 = numbers.fill(6)
+
+console.assert(numbers.length === numbers1.length, 'numbers length is 4')
+console.assert(numbers[0] === 6, 'numbers at 0 is 6')
+console.assert(numbers[1] === 6, 'numbers at 1 is 6')
+console.assert(numbers[2] === 6, 'numbers at 2 is 6')
+console.assert(numbers[3] === 6, 'numbers at 3 is 6')
+
+console.info('CASE fill with 2 parameter in Curray')
+var numbers = new Curray(1, 2, 3, 4)
+var numbers1 = numbers.fill(5, 1)
+
+console.assert(numbers.length === numbers1.length, 'numbers length is 4')
+console.assert(numbers[0] === 1, 'numbers at 0 is 1')
+console.assert(numbers[1] === 5, 'numbers at 1 is 5')
+console.assert(numbers[2] === 5, 'numbers at 2 is 5')
+console.assert(numbers[3] === 5, 'numbers at 3 is 5')
+
+console.info('CASE fill with 3 parameter in Curray')
+var numbers = new Curray(1, 2, 3, 4)
+var numbers1 = numbers.fill(0, 2, 3)
+
+console.assert(numbers.length === numbers1.length, 'numbers length is 4')
+console.assert(numbers[0] === 1, 'numbers at 0 is 1')
+console.assert(numbers[1] === 2, 'numbers at 1 is 2')
+console.assert(numbers[2] === 0, 'numbers at 2 is 0')
+console.assert(numbers[3] === 4, 'numbers at 3 is 4')
