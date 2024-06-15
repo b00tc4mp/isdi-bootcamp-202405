4219bbf0c8edc08bbb1c6be0e06191d5ea5c821b
Bruno Ornella <brunoornella228@gmail.com>
2024-06-15 12:41:50 +0200

    add logic #67

---
+++ staff/bruno-ornella/ponies/app/logic/createPost.js
@@ -0,0 +1,16 @@
+function createPost(image, caption) {
+    if (!image.startsWith('http'))
+        throw new Error('invalid image')
+
+    var posts = localStorage.posts !== undefined ? JSON.parse(localStorage.posts) : []
+
+    var post = {
+        image: image,
+        caption: caption,
+        author: sessionStorage.username,
+        date: new Date().toDateString()
+    }
+    posts.push(post)
+
+    localStorage.posts = JSON.stringify(posts)
+}
---
+++ staff/bruno-ornella/ponies/app/logic/registerUser.js
@@ -0,0 +1,51 @@
+var EMAIL_REGEX = /^[a-z0-9._]+@[a-z0-9.-]{3,63}\.[a-z]{2,10}$/
+function registerUser(name, surname, email, username, password, passwordRepeat) {
+
+
+    if (name.trim() === '')
+        throw new Error('invalid name')
+
+    if (surname.trim().length < 2)
+        throw new Error('invalid surname')
+
+    if (!EMAIL_REGEX.test(email))
+        throw new Error('invalid email')
+
+    if (username.trim().length < 4)
+        throw new Error('invalid username')
+
+    if (password.trim().length < 8)
+        throw new Error('invalid password')
+
+    if (password !== passwordRepeat)
+        throw new Error('passwords do not match')
+
+    var users = localStorage.users !== undefined ? JSON.parse(localStorage.users) : []
+
+    var user = users.find(function (user) {
+        return user.mail === email
+    })
+
+    if (user !== undefined)
+        alert('email already exits')
+
+    var user = users.find(function (user) {
+        return user.username === username
+    })
+
+    if (user !== undefined)
+        alert('username already exits')
+
+    user = {
+        name: name,
+        surname: surname,
+        email: email,
+        username: username,
+        password: password
+    }
+
+
+    users.push(user)
+
+    localStorage.users = JSON.stringify(users)
+}
