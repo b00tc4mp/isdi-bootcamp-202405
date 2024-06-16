c41a71a5de9a0017d890295f664c04e987e4a039
root <root@MSI.>
2024-06-16 15:54:50 +0200

    implement all ponies app until delete post #60

---
+++ staff/lucas-orts/ponies/app/logic/createPost.js
@@ -0,0 +1,19 @@
+function createPost(image, caption) {
+    if (!image.startsWith('http')) {
+        throw new Error('invalid image')
+    }
+
+    var posts = localStorage.posts !== undefined ? JSON.parse(localStorage.posts) : []
+
+    var post = {
+        image: image,
+        caption: caption,
+        author: sessionStorage.username,
+        date: new Date().toISOString(),
+        id: generateRandomId()
+    }
+
+    posts.push(post)
+
+    localStorage.posts = JSON.stringify(posts)
+}
