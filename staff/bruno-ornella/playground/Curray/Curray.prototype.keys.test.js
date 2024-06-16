eebb2692e0474c82cfd8d45dd8999a90c3a384f6
Bruno Ornella <brunoornella228@gmail.com>
2024-06-16 12:12:47 +0200

    add news Curray #29

---
+++ staff/bruno-ornella/playground/Curray/Curray.prototype.keys.test.js
@@ -0,0 +1,31 @@
+var Curray = require('./Curray')
+require('./Curray.prototype.keys')
+
+console.info('TEST Curray.prototype.keys.js')
+
+console.info('CASE keys in Curray')
+
+var letters = new Curray('a', 'b', 'c')
+var iterator = letters.keys()
+
+var next = iterator.next()
+
+console.assert(next.value === 0, 'next value is 0')
+console.assert(next.done === false, 'next done is false')
+
+next = iterator.next()
+
+console.assert(next.value === 1, 'next value is 1')
+console.assert(next.done === false, 'next done is false')
+
+next = iterator.next()
+
+console.assert(next.value === 2, 'next value is 2')
+console.assert(next.done === false, 'next done is false')
+
+next = iterator.next()
+
+console.assert(next.value === undefined, 'next value is undefined')
+console.assert(next.done === true, 'next done is true')
+
+
