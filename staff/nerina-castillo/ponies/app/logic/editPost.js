3a492d552fcde47c44708658f0d2da016d4d90da
Nerina Castillo <nerina.cgomez@gmail.com>
2024-06-18 20:40:31 +0200

    add layout styles to ponies #68

--- staff/nerina-castillo/ponies/app/logic/editPost.js
+++
@@ -1,8 +0,0 @@
-function editPost(id) {
-  var posts = getAllPosts();
-  var index = posts.findIndex(function (element) {
-    return element.id === id;
-  });
-
-  localStorage.posts = JSON.stringify(posts);
-}
