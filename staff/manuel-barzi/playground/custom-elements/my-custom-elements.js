6d32e88aa92515bcbcab0af41c827edcb3043cb8
Manuel Barzi <manuelbarzi@gmail.com>
2024-06-19 13:32:29 +0200

    implement curray in es6; implement animal and human in oop; implement custom elements; #58

---
+++ staff/manuel-barzi/playground/custom-elements/my-custom-elements.js
@@ -0,0 +1,25 @@
+class HelloWorld extends HTMLElement {
+    constructor() {
+        super()
+    }
+
+    connectedCallback() {
+        const h1 = document.createElement('h1')
+
+        h1.innerText = 'Hello, World!'
+
+        this.appendChild(h1)
+
+        h1.style.backgroundColor = 'red'
+    }
+}
+
+window.customElements.define('hello-world', HelloWorld)
+
+class HelloTo extends HTMLElement {
+    constructor() {
+        super()
+    }
+}
+
+window.customElements.define('hello-world', HelloTo)
