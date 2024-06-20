21e6f3dbce2f8d91da5fa202f0252d5af1dcec6a
Liliana De Ponte <lilideponte@gmail.com>
2024-06-20 17:05:15 +0200

    implement custom-elements; implement custom web-components #24

---
+++ staff/liliana-de-ponte/playground/traverse-dom/traverse.js
@@ -0,0 +1,38 @@
+function traverse(element) {
+    var elementTree = ''
+
+    function loop(element) {
+        var parentCount = 0
+        var parent = element.parentElement
+
+        while (parent !== null) {
+            parentCount++
+
+            parent = parent.parentElement
+
+        }
+
+        // if (element.tagName && element.tagName.includes('-'))
+        //     alert('custom element detected: ' + element.tagName)
+
+        // console.log('\t'.repeat(parentCount) + element.tagName)
+
+        elementTree += '\t'.repeat(parentCount) + element.tagName + '\n'
+
+        var children = element.children
+
+        for (var i = 0; i < children.length; i++) {
+            var child = children[i]
+
+            loop(child)
+        }
+
+    }
+
+    loop(element)
+
+    return elementTree
+
+}
+
+console.log(traverse(document))
---
+++ staff/liliana-de-ponte/playground/web-components/main2.js
@@ -0,0 +1,54 @@
+class Component {
+    constructor(container) {
+        this.container = container
+    }
+
+    add(child) {
+        if (!(child instanceof Component))
+            throw new TyperError('child is not a Component')
+        this.container.appendChild(child.container)
+    }
+
+    setText(text) {
+        if (typeof text !== 'string')
+            throw new TypeError('text is not a string')
+
+        this.container.innerText = text
+    }
+
+    setBackgroundColor(color) {
+        if (typeof color !== 'string')
+            throw new TypeError('color is not a string')
+
+        this.container.style.backgroundColor = color
+    }
+
+    setColor(color) {
+        if (typeof color !== 'string')
+            throw new TypeError('color is not a string')
+
+        this.container.style.color = color
+    }
+}
+
+const view = new Component(document.body)
+view.setBackgroundColor('black')
+
+const colorList = new Component(document.createElement('ul'))
+
+const redColorItem = new Component(document.createElement('li'))
+redColorItem.setText('Red')
+redColorItem.setColor('tomato')
+colorList.add(redColorItem)
+
+const blueColorItem = new Component(document.createElement('li'))
+blueColorItem.setText('Blue')
+blueColorItem.setColor('dodgerBlue')
+colorList.add(blueColorItem)
+
+const yellowColorItem = new Component(document.createElement('li'))
+yellowColorItem.setText('yellow')
+yellowColorItem.setColor('gold')
+colorList.add(yellowColorItem)
+
+view.container.appendChild(colorList.container)
