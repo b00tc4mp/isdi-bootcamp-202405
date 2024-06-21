3c5b91267da191022058c5ac9f3113f1f532826b
Manuel Barzi <manuelbarzi@gmail.com>
2024-06-21 10:39:28 +0200

    implement first test cases for array / curray flat method #23

--- staff/manuel-barzi/playground/Curray/es6/Curray.4.js
+++ staff/manuel-barzi/playground/Curray/es6/Curray.4.js
@@ -116,44 +116,6 @@ class Curray {
             }
 
             return flatted
-        } else if (depth === 4) {
-            const flatted = new Curray()
-
-            for (let i = 0; i < this.length; i++) {
-                const element = this[i]
-
-                if (!(element instanceof Curray))
-                    flatted[flatted.length++] = element
-                else
-                    for (let i = 0; i < element.length; i++) {
-                        const elem = element[i]
-
-                        if (!(elem instanceof Curray))
-                            flatted[flatted.length++] = elem
-                        else
-                            for (let i = 0; i < elem.length; i++) {
-                                const elem1 = elem[i]
-
-                                if (!(elem1 instanceof Curray))
-                                    flatted[flatted.length++] = elem1
-                                else
-                                    for (let i = 0; i < elem1.length; i++) {
-                                        const elem2 = elem1[i]
-
-                                        if (!(elem2 instanceof Curray))
-                                            flatted[flatted.length++] = elem2
-                                        else
-                                            for (let i = 0; i < elem2.length; i++) {
-                                                const elem3 = elem2[i]
-
-                                                flatted[flatted.length++] = elem3
-                                            }
-                                    }
-                            }
-                    }
-            }
-
-            return flatted
         }
     }
 }
