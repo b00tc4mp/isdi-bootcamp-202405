993cd50807744d866c47282385689061df3168ce
Manuel Barzi <manuelbarzi@gmail.com>
2024-06-21 12:09:51 +0200

    refactor flat method applying recursion #23

--- staff/manuel-barzi/playground/Curray/es6/Curray.js
+++ staff/manuel-barzi/playground/Curray/es6/Curray.js
@@ -43,118 +43,23 @@ class Curray {
         return mapped
     }
 
-    flat(depth) {
-        if (depth === undefined) {
-            const flatted = new Curray()
+    flat(depth = 1) {
+        const flatted = new Curray()
 
-            for (let i = 0; i < this.length; i++) {
-                const element = this[i]
+        function loop(curray, count) {
+            for (let i = 0; i < curray.length; i++) {
+                const element = curray[i]
 
-                if (!(element instanceof Curray))
+                if (!(element instanceof Curray) || count === depth)
                     flatted[flatted.length++] = element
                 else
-                    for (let i = 0; i < element.length; i++) {
-                        const elem = element[i]
-
-                        flatted[flatted.length++] = elem
-                    }
+                    loop(element, count + 1)
             }
-
-            return flatted
-        } else if (depth === 2) {
-            const flatted = new Curray()
-
-            for (let i = 0; i < this.length; i++) {
-                const element = this[i]
-
-                if (!(element instanceof Curray))
-                    flatted[flatted.length++] = element
-                else
-                    for (let i = 0; i < element.length; i++) {
-                        const elem = element[i]
-
-                        if (!(elem instanceof Curray))
-                            flatted[flatted.length++] = elem
-                        else
-                            for (let i = 0; i < elem.length; i++) {
-                                const elem1 = elem[i]
-
-                                flatted[flatted.length++] = elem1
-                            }
-                    }
-            }
-
-            return flatted
-        } else if (depth === 3 || depth === Infinity) {
-            const flatted = new Curray()
-
-            for (let i = 0; i < this.length; i++) {
-                const element = this[i]
-
-                if (!(element instanceof Curray))
-                    flatted[flatted.length++] = element
-                else
-                    for (let i = 0; i < element.length; i++) {
-                        const elem = element[i]
-
-                        if (!(elem instanceof Curray))
-                            flatted[flatted.length++] = elem
-                        else
-                            for (let i = 0; i < elem.length; i++) {
-                                const elem1 = elem[i]
-
-                                if (!(elem1 instanceof Curray))
-                                    flatted[flatted.length++] = elem1
-                                else
-                                    for (let i = 0; i < elem1.length; i++) {
-                                        const elem2 = elem1[i]
-
-                                        flatted[flatted.length++] = elem2
-                                    }
-                            }
-                    }
-            }
-
-            return flatted
-        } else if (depth === 4) {
-            const flatted = new Curray()
-
-            for (let i = 0; i < this.length; i++) {
-                const element = this[i]
-
-                if (!(element instanceof Curray))
-                    flatted[flatted.length++] = element
-                else
-                    for (let i = 0; i < element.length; i++) {
-                        const elem = element[i]
-
-                        if (!(elem instanceof Curray))
-                            flatted[flatted.length++] = elem
-                        else
-                            for (let i = 0; i < elem.length; i++) {
-                                const elem1 = elem[i]
-
-                                if (!(elem1 instanceof Curray))
-                                    flatted[flatted.length++] = elem1
-                                else
-                                    for (let i = 0; i < elem1.length; i++) {
-                                        const elem2 = elem1[i]
-
-                                        if (!(elem2 instanceof Curray))
-                                            flatted[flatted.length++] = elem2
-                                        else
-                                            for (let i = 0; i < elem2.length; i++) {
-                                                const elem3 = elem2[i]
-
-                                                flatted[flatted.length++] = elem3
-                                            }
-                                    }
-                            }
-                    }
-            }
-
-            return flatted
         }
+
+        loop(this, 0)
+
+        return flatted
     }
 }
 
