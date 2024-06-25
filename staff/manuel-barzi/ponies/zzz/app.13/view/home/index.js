f1001969acc8a376eba78a5db48ceb6f95a646a4
Manuel Barzi <manuelbarzi@gmail.com>
2024-06-25 10:49:02 +0200

    add button compo; remove iffes and use scoped blocks; add doc with class diagram for compos; #58

--- staff/manuel-barzi/ponies/zzz/app.13/view/home/index.js
+++ staff/manuel-barzi/ponies/zzz/app.13/view/home/index.js
@@ -1,12 +1,12 @@
-{
+(function () {
     const body = new Component(document.body)
     const header = new Header
     body.add(header)
 
-    const logoutButton = new Button
-    logoutButton.setClassName('logout-button')
-    logoutButton.setText('Logout')
-    header.add(logoutButton)
+    var logoutButton = document.createElement('button')
+    logoutButton.className = 'logout-button'
+    logoutButton.innerText = 'Logout'
+    header.container.appendChild(logoutButton)
 
     logoutButton.onclick = function () {
         try {
@@ -19,17 +19,17 @@
     }
 
 
-    const main = document.createElement('main')
+    var main = document.createElement('main')
     main.className = 'view main'
     document.body.appendChild(main)
 
-    const postListSection = document.createElement('section')
+    var postListSection = document.createElement('section')
     postListSection.className = 'post-list'
     main.appendChild(postListSection)
 
     function clearPosts() {
-        for (let i = postListSection.children.length - 1; i > -1; i--) {
-            const child = postListSection.children[i]
+        for (var i = postListSection.children.length - 1; i > -1; i--) {
+            var child = postListSection.children[i]
 
             postListSection.removeChild(child)
         }
@@ -37,34 +37,34 @@
 
     function listPosts() {
         try {
-            const posts = getAllPosts()
+            var posts = getAllPosts()
 
             posts.forEach(function (post) {
-                const postArticle = document.createElement('article')
+                var postArticle = document.createElement('article')
                 postArticle.className = 'post'
                 postListSection.appendChild(postArticle)
 
-                const postAuthorTitle = document.createElement('h3')
+                var postAuthorTitle = document.createElement('h3')
                 postAuthorTitle.className = 'post__author'
                 postAuthorTitle.innerText = post.author
                 postArticle.appendChild(postAuthorTitle)
 
-                const postImage = document.createElement('img')
+                var postImage = document.createElement('img')
                 postImage.className = 'post__image'
                 postImage.src = post.image
                 postArticle.appendChild(postImage)
 
-                const postCaptionText = document.createElement('p')
+                var postCaptionText = document.createElement('p')
                 postCaptionText.className = 'post__caption'
                 postCaptionText.innerText = post.caption
                 postArticle.appendChild(postCaptionText)
 
                 if (post.author === getUserUsername()) {
-                    const postActionButtonsDiv = document.createElement('div')
+                    var postActionButtonsDiv = document.createElement('div')
                     postActionButtonsDiv.className = 'post__actions'
                     postArticle.appendChild(postActionButtonsDiv)
 
-                    const postDeleteButton = document.createElement('button')
+                    var postDeleteButton = document.createElement('button')
                     postDeleteButton.innerText = 'Delete'
                     postActionButtonsDiv.appendChild(postDeleteButton)
 
@@ -85,30 +85,30 @@
                             }
                     }
 
-                    const editButton = document.createElement('button')
+                    var editButton = document.createElement('button')
                     editButton.innerText = 'Edit'
                     postActionButtonsDiv.appendChild(editButton)
 
                     editButton.onclick = function () {
 
-                        const editCaptionForm = document.createElement('form')
+                        var editCaptionForm = document.createElement('form')
                         postArticle.appendChild(editCaptionForm)
 
-                        const editCaptionLabel = document.createElement('label')
+                        var editCaptionLabel = document.createElement('label')
                         editCaptionLabel.htmlFor = 'edit-caption-input'
                         editCaptionForm.appendChild(editCaptionLabel)
 
-                        const editCaptionInput = document.createElement('input')
+                        var editCaptionInput = document.createElement('input')
                         editCaptionInput.id = editCaptionLabel.htmlFor
                         editCaptionInput.value = post.caption
                         editCaptionForm.appendChild(editCaptionInput)
 
-                        const editCaptionSubmitButton = document.createElement('button')
+                        var editCaptionSubmitButton = document.createElement('button')
                         editCaptionSubmitButton.type = 'submit'
                         editCaptionSubmitButton.innerText = 'Save'
                         editCaptionForm.appendChild(editCaptionSubmitButton)
 
-                        const editCaptionCancelButton = document.createElement('button')
+                        var editCaptionCancelButton = document.createElement('button')
                         editCaptionCancelButton.innerText = 'Cancel'
                         editCaptionCancelButton.type = 'button'
                         editCaptionForm.appendChild(editCaptionCancelButton)
@@ -121,7 +121,7 @@
                             event.preventDefault()
 
                             try {
-                                const newCaption = editCaptionInput.value
+                                var newCaption = editCaptionInput.value
 
                                 updatePostCaption(post.id, newCaption)
 
@@ -141,7 +141,7 @@
                     }
                 }
 
-                const postDateTime = document.createElement('time')
+                var postDateTime = document.createElement('time')
                 postDateTime.className = 'post__time'
                 postDateTime.innerText = formatTime(new Date(post.date))
                 postArticle.appendChild(postDateTime)
@@ -153,35 +153,35 @@
 
     listPosts()
 
-    const footer = document.createElement('footer')
+    var footer = document.createElement('footer')
     footer.className = 'footer'
     document.body.appendChild(footer)
 
-    const addPostButton = document.createElement('button')
+    var addPostButton = document.createElement('button')
     addPostButton.className = 'add-post-button'
     addPostButton.innerText = '+'
     footer.appendChild(addPostButton)
 
     addPostButton.onclick = function () {
-        const createPostSection = document.createElement('section')
+        var createPostSection = document.createElement('section')
         createPostSection.className = 'create-post-section'
         footer.appendChild(createPostSection)
 
-        const createPostTitle = document.createElement('h2')
+        var createPostTitle = document.createElement('h2')
         createPostTitle.className = 'create-post-section__title'
         createPostTitle.innerText = 'Create Post'
         createPostSection.appendChild(createPostTitle)
 
-        const createPostForm = document.createElement('form')
+        var createPostForm = document.createElement('form')
         createPostForm.className = 'form'
         createPostSection.appendChild(createPostForm)
 
         createPostForm.onsubmit = function (event) {
             event.preventDefault()
 
-            //const postImageInput = document.getElementById('post-image-input')
-            const postImage = postImageInput.value
-            const postCaption = postCaptionInput.value
+            //var postImageInput = document.getElementById('post-image-input')
+            var postImage = postImageInput.value
+            var postCaption = postCaptionInput.value
 
             try {
                 createPost(postImage, postCaption)
@@ -195,45 +195,45 @@
             }
         }
 
-        const postImageFieldDiv = document.createElement('div')
+        var postImageFieldDiv = document.createElement('div')
         postImageFieldDiv.className = 'form__field'
         createPostForm.appendChild(postImageFieldDiv)
 
-        const postImageLabel = document.createElement('label')
+        var postImageLabel = document.createElement('label')
         postImageLabel.htmlFor = 'post-image-input'
         postImageLabel.innerText = 'Image'
         postImageFieldDiv.appendChild(postImageLabel)
 
-        const postImageInput = document.createElement('input')
+        var postImageInput = document.createElement('input')
         postImageInput.className = 'form__input'
         postImageInput.id = postImageLabel.htmlFor
         postImageFieldDiv.appendChild(postImageInput)
 
-        const postCaptionFieldDiv = document.createElement('div')
+        var postCaptionFieldDiv = document.createElement('div')
         postCaptionFieldDiv.className = 'form__field'
         createPostForm.appendChild(postCaptionFieldDiv)
 
-        const postCaptionLabel = document.createElement('label')
+        var postCaptionLabel = document.createElement('label')
         postCaptionLabel.htmlFor = 'post-caption-input'
         postCaptionLabel.innerText = 'Caption'
         postCaptionFieldDiv.appendChild(postCaptionLabel)
 
-        const postCaptionInput = document.createElement('input')
+        var postCaptionInput = document.createElement('input')
         postCaptionInput.className = 'form__input'
         postCaptionInput.id = postCaptionLabel.htmlFor
         postCaptionFieldDiv.appendChild(postCaptionInput)
 
-        const postButtonsDiv = document.createElement('div')
+        var postButtonsDiv = document.createElement('div')
         postButtonsDiv.className = 'create-post-section__buttons'
         createPostForm.appendChild(postButtonsDiv)
 
-        const postSubmitButton = document.createElement('button')
+        var postSubmitButton = document.createElement('button')
         postSubmitButton.className = 'form__button'
         postSubmitButton.type = 'submit'
         postSubmitButton.innerText = 'Create'
         postButtonsDiv.appendChild(postSubmitButton)
 
-        const postCancelButton = document.createElement('button')
+        var postCancelButton = document.createElement('button')
         postCancelButton.className = 'form__button'
         postCancelButton.type = 'reset'
         postCancelButton.innerText = 'Cancel'
@@ -243,4 +243,4 @@
             footer.removeChild(createPostSection)
         }
     }
-}
+})()
--- staff/manuel-barzi/ponies/zzz/app.13/view/login/index.js
+++ staff/manuel-barzi/ponies/zzz/app.13/view/login/index.js
@@ -1,14 +1,14 @@
-{
-    const loginForm = new Form('form')
+(function () {
+    var loginForm = new Form('form')
 
     loginForm.onSubmit(function (event) {
         event.preventDefault()
 
-        const usernameInput = document.getElementById('username-input')
-        const passwordInput = document.getElementById('password-input')
+        var usernameInput = document.getElementById('username-input')
+        var passwordInput = document.getElementById('password-input')
 
-        const username = usernameInput.value
-        const password = passwordInput.value
+        var username = usernameInput.value
+        var password = passwordInput.value
 
         try {
             loginUser(username, password)
@@ -19,11 +19,11 @@
         }
     })
 
-    const registerLink = new Link('a')
+    var registerLink = new Link('a')
 
     registerLink.onClick(function (event) {
         event.preventDefault()
 
         location.href = '../register'
     })
-}
+})()
