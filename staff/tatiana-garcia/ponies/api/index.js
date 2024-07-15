7dbeec0fa40d8d6c84c56d843fc26922071e1b8b
Tatiana_Garcia <151873298+TatiGV@users.noreply.github.com>
2024-07-15 11:53:18 +0200

    implement comments in index.js #62

--- staff/tatiana-garcia/ponies/api/index.js
+++ staff/tatiana-garcia/ponies/api/index.js
@@ -1,9 +1,13 @@
+// Express -> marco de trabajo para construir aplicaciones web y APIs en node.js. Facilita la creacion de servidores y el manejo de solicitudes HTTP
 import express from 'express'
 
-const api = express()
+const api = express() // Se crea una instancia de una aplicacion express, esta instancia se usará para configurar nuestras rutas y manejar las solicitudes HTTP.
 
 api.get('/hello', (req, res) => {
-    res.send('Hello, World!')
+    //aqui se define una ruta GET en '\hello' para que el servidor responda con Hello, World!' cuando alguien acceda a '\hello'. 
+    //req (request)-> objeto de solicitud que contiene info sobre la solicitud HTTP.
+    //res (response) -> objeto de respuesta que usamos para enviar una respuesta al cliente.
+    res.send('Hello, World!') //se envia la respuesta 'Hello, World!'
 })
 
-api.listen(8080, () => console.log('server up'))
+api.listen(8080, () => console.log('server up')) // se inicia el servidor
