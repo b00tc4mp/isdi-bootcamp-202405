363d2750c8865722e106ffec3ec2edbfa7db744b
Samu2196 <samuelespinetti@hotmail.com>
2024-07-16 09:52:31 +0200

    implement register method in server #66

---
+++ staff/samuele-spinetti/ponies/api/test/register.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d '{"name":"Pepito","surname":"grillo","email":"pepito@grillo.com","username":"pepitogrillo","password":"11111111"}' http://localhost:8080/register/user -v
