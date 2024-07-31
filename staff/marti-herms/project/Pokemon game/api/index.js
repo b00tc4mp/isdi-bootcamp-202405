1826dd9efb52370407b072fd47886c55ff5dce4e
Marti Herms <mhermsrubio@gmail.com>
2024-07-31 22:06:58 +0200

    implement some improvements in the API index #84

--- staff/marti-herms/project/Pokemon game/api/index.js
+++ staff/marti-herms/project/Pokemon game/api/index.js
@@ -4,76 +4,74 @@ import mongoose from 'mongoose'
 
 import fs from 'fs'
 
-mongoose.connect('127.0.0.1:27017')
-    .then(() => {
-
-    })
-// const apiUrl = 'https://pokeapi.co/api/v2/pokemon'
-const api = express()
 const port = 8080
 
-api.get('/api/data', async (req, res) => {
-    try {
-        // Replace the URL with the endpoint of the public API you want to fetch data from
-        const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
+mongoose.connect('127.0.0.1:27017')
+    .then(() => {
+        // const apiUrl = 'https://pokeapi.co/api/v2/pokemon'
+        const api = express()
 
-        // Make a GET request to the public API
-        const response = await axios.get(apiUrl)
+        api.get('/api/data', async (req, res) => {
+            try {
+                const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0'
 
-        const json = JSON.stringify(response.data)
+                const response = await axios.get(apiUrl)
 
-        fs.writeFileSync('./pokemonList.json', json)
+                const json = JSON.stringify(response.data)
 
-        // Send the API response data back to the client
-        res.json(response.data)
-    } catch (error) {
-        console.error('Error fetching data from API:', error)
-        res.status(500).json({ error: 'An error occurred while fetching data.' })
-    }
-})
 
-api.get('/api/pokemon', async (req, res) => {
-    const jsonRead = fs.readFileSync('./pokemonList.json', 'utf-8')
 
-    const { results } = JSON.parse(jsonRead)
+                res.json(response.data)
+            } catch (error) {
+                console.error('Error fetching data from API:', error)
+                res.status(500).json({ error: 'An error occurred while fetching data.' })
+            }
+        })
 
-    const names = results.map(pokemon => pokemon.name)
+        api.get('/api/pokemon', async (req, res) => {
+            const jsonRead = fs.readFileSync('./pokemonList.json', 'utf-8')
 
-    const names1 = names.slice(0, 151)
+            const { results } = JSON.parse(jsonRead)
 
-    names1.forEach(async pokemonName => {
-        const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+            const names = results.map(pokemon => pokemon.name)
 
-        const response = await axios.get(apiUrl)
+            const names1 = names.slice(0, 151)
 
-        const pokemon = response.data
+            names1.forEach(async pokemonName => {
+                const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
 
-        delete pokemon.base_experience
-        delete pokemon.cries
-        delete pokemon.game_indices
-        delete pokemon.height
-        delete pokemon.held_items
-        delete pokemon.is_default
-        delete pokemon.location_area_encounters
-        delete pokemon.order
-        delete pokemon.past_abilities
-        delete pokemon.past_type
-        delete pokemon.species
+                const response = await axios.get(apiUrl)
 
-        const jsonWrite = JSON.stringify(response.data)
+                const pokemon = response.data
 
-        fs.appendFile('./pokemon.json', jsonWrite, (error) => {
-            if (error) throw new Error;
+                delete pokemon.base_experience
+                delete pokemon.cries
+                delete pokemon.game_indices
+                delete pokemon.height
+                delete pokemon.held_items
+                delete pokemon.is_default
+                delete pokemon.location_area_encounters
+                delete pokemon.order
+                delete pokemon.past_abilities
+                delete pokemon.past_type
+                delete pokemon.species
 
-            console.log('It\'s saved!');
+                const jsonWrite = JSON.stringify(response.data)
+
+                fs.appendFile('./pokemon.json', jsonWrite, (error) => {
+                    if (error) throw new Error;
+
+                    console.log('It\'s saved!');
+                })
+            })
+
+
+            res.send()
+        })
+
+
+        api.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`)
         })
     })
-
-
-    res.send()
-})
-
-
-api.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`)
-})
+    .catch(error => console.error(error))
