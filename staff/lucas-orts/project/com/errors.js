13925a75035ebc1882e97bde2af774fd697bea50
Lucas Orts <postulento@hotmail.com>
2024-08-11 12:38:43 +0200

    create validate and errors in com

---
+++ staff/lucas-orts/project/com/errors.js
@@ -0,0 +1,31 @@
+class ValidationError extends error {
+    constructor(message) {
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+class DuplicityError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+class SystemError extends Error {
+    constructor(message) {
+        super(message)
+
+        this.name = this.constructor.name
+    }
+}
+
+const errors = {
+    ValidationError,
+    DuplicityError,
+    SystemError
+}
+
+export default errors
---
+++ staff/lucas-orts/project/com/validate.js
@@ -0,0 +1,35 @@
+import errors from './errors.js'
+
+const { ValidationError } = errors
+
+const NAME_REGEX = /^(?!.*\s{2})[a-zA-Z ]{3,16}$/
+const EMAIL_REGEX = /^[a-z0-9._]+@[a-z0-9.-]{3,63}\.[a-z]{2,10}$/
+
+function validateString(value, explain = 'value') {
+    if (typeof value !== 'string') throw new ValidationError(`${explain} is not a string`)
+}
+
+function validatePassword(password) {
+    validateString(password, 'password')
+    if (password.trim().length < 8) throw new ValidationError('password length is lower than 8 characters')
+    if (password.includes(' ')) throw new ValidationError('password has empty spaces')
+}
+
+function validateName(name, explain = 'name') {
+    validateString(name, explain)
+    if (!NAME_REGEX.test(name)) throw new ValidationError(`invalid ${explain}`)
+}
+
+function validateEmail(email) {
+    validateString(email, 'email')
+    if (!EMAIL_REGEX.test(email)) throw new ValidationError(`invalid email`)
+}
+
+const validate = {
+    string: validateString,
+    password: validatePassword,
+    name: validateName,
+    email: validateEmail
+}
+
+export default validate
