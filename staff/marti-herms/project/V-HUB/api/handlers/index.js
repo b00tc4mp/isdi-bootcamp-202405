0dbd65de9cfd3e0c41f0d89252d8a82812f1506b
Marti Herms <mhermsrubio@gmail.com>
2024-08-13 11:16:16 +0200

    implement search game logic in api #84

--- staff/marti-herms/project/V-HUB/api/handlers/index.js
+++ staff/marti-herms/project/V-HUB/api/handlers/index.js
@@ -2,12 +2,14 @@ import authenticateUserHandler from './authenticateUserHandler.js'
 import registerUserHandler from './registerUserHandler.js'
 import getUserUsernameHandler from './getUserUsernameHandler.js'
 import registerGameHandler from './registerGameHandler.js'
+import searchGameHandler from './searchGameHandler.js'
 
 const handle = {
     authenticateUser: authenticateUserHandler,
     registerUser: registerUserHandler,
     getUserUsername: getUserUsernameHandler,
-    registerGame: registerGameHandler
+    registerGame: registerGameHandler,
+    searchGame: searchGameHandler
 }
 
 export default handle
