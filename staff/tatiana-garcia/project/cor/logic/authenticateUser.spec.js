f7eb86d7f7f4887112ec13f1b337245bcc686e80
Tatiana_Garcia <151873298+TatiGV@users.noreply.github.com>
2024-08-13 14:32:33 +0200

    implement authenticateUser.spec.js test #99

---
+++ staff/tatiana-garcia/project/cor/logic/authenticateUser.spec.js
@@ -0,0 +1,117 @@
+import 'dotenv/config'
+import mongoose from 'mongoose'
+import { expect } from 'chai'
+import bcrypt from 'bcryptjs'
+
+import authenticateUser from './authenticateUser.js'
+import { User } from '../data/models.js'
+
+import { errors } from '../../com/index.js'
+
+const { NotFoundError, CredentialsError, ValidationError } = errors
+
+describe('authenticateUser', () => {
+    before(() => mongoose.connect(process.env.MONGODB_URI))
+
+    beforeEach(() => User.deleteMany())
+
+    it('succeds on username and password is correct', () => {
+        bcrypt.hash('123123123', 8)
+            .then(hash => User.create({ image: 'https://www.ngenespanol.com/', name: 'Tatiana', surname: 'Garcia', email: 'tati@garcia.com', username: 'tatig', password: '123123123', passwordRepeat: '123123123', role: 'user', petsitterName: 'elsaltres', city: 'Barcelona', description: 'estoy hasta las pelotas', pets: ['conejo, cobaya'] }))
+            .then(() => authenticateUser('tatig', '123123123'))
+            .then(value => expect(value).to.be.string)
+
+    })
+
+    it('fails on non-existing user', () => {
+        let _error
+
+        authenticateUser('tatiggg', '123123123')
+            .catch(error => _error = error)
+            .finally(() => {
+                expect(_error).to.be.instanceOf(NotFoundError)
+                expect(_error.message).to.equal('user not found')
+            })
+    })
+
+    it('fails on passwords do not match', () => {
+        let _error
+
+        return User.create({ image: 'https://www.ngenespanol.com/', name: 'Tatiana', surname: 'Garcia', email: 'tati@garcia.com', username: 'tatig', password: '123123123', passwordRepeat: '123123123', role: 'user', petsitterName: 'elsaltres', city: 'Barcelona', description: 'estoy hasta las pelotas', pets: ['conejo, cobaya'] })
+            .then(user => authenticateUser(user.username, '123123223'))
+            .catch(error => _error = error)
+            .finally(() => {
+                expect(_error).to.be.instanceOf(CredentialsError)
+                expect(_error.message).to.equal('wrong password')
+            })
+    })
+
+    it('fails on non string username', () => {
+        let error
+
+        try {
+            authenticateUser(123, '123123123')
+        } catch (_error) {
+            error = _error
+        } finally {
+            expect(error).to.be.instanceOf(ValidationError)
+            expect(error.message).to.equal('username is not a string')
+        }
+    })
+
+    it('fails on invalid username', () => {
+        let error
+
+        try {
+            authenticateUser('', '123123123')
+        } catch (_error) {
+            error = _error
+        } finally {
+            expect(error).to.be.instanceOf(ValidationError)
+            expect(error.message).to.equal('invalid username')
+        }
+    })
+
+    it('fails on non string password', () => {
+        let error
+
+        try {
+            authenticateUser('tatig', 123123123)
+        } catch (_error) {
+            error = _error
+        } finally {
+            expect(error).to.be.instanceOf(ValidationError)
+            expect(error.message).to.equal('password is not a string')
+        }
+    })
+
+    it('fails on password is too short', () => {
+        let error
+
+        try {
+            authenticateUser('tatig', '123')
+        } catch (_error) {
+            error = _error
+        } finally {
+            expect(error).to.be.instanceOf(ValidationError)
+            expect(error.message).to.equal('password length is lower than 8 characters')
+        }
+    })
+
+    it('fails on password with spaces', () => {
+        let error
+
+        try {
+            authenticateUser('tatig', '123123 123')
+        } catch (_error) {
+            error = _error
+        } finally {
+            expect(error).to.be.instanceOf(ValidationError)
+            expect(error.message).to.equal('password has empty spaces')
+        }
+    })
+
+    afterEach(() => User.deleteMany())
+
+    after(() => mongoose.disconnect())
+})
