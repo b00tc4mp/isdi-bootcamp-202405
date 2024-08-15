84167586fdd8bfc2ac8c5cedef2c69d1562c13ce
Tatiana_Garcia <151873298+TatiGV@users.noreply.github.com>
2024-08-15 19:18:07 +0200

    modified Routes in app.jsx #99

--- staff/tatiana-garcia/project/app/view/App.jsx
+++ staff/tatiana-garcia/project/app/view/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
 import Register from './register'
 import Login from './login/index.jsx'
 import Home from './home/index.jsx'
+import Contact from './contact/index.jsx'
 
 import logic from '../logic/index.js'
 
@@ -15,13 +16,21 @@ export default function App() {
 
   const handleRegister = () => { navigate('/login') }
 
+  // const handleContact = () => { navigate('/contact') }
+
   const handleLoginClick = () => { navigate('/login') }
 
-  const handleLogout = () => { navigate('/login') }
+  const handleLogout = () => {
+    logic.logoutUser()
+
+    navigate('/login')
+  }
 
   return <Routes>
-    <Route path="/login" element={logic.isUserLoggedIn() ? <Navigate to="/" /> : <Login onLogin={handleLogin} onRegisterClick={handleRegisterClick} />} />
-    <Route path="/register" element={logic.isUserLoggedIn() ? <Navigate to="/" /> : <Register onRegister={handleRegister} onLoginClick={handleLoginClick} />} />
-    <Route path="/*" element={logic.isUserLoggedIn() ? <Home onLogout={handleLogout} /> : <Navigate to="/login" />} />
+    <Route path='/*' element={<Home onLogout={handleLogout} />} />
+    <Route path='/login' element={logic.isUserLoggedIn() ? <Navigate to='/' /> : <Login onLogin={handleLogin} onRegisterClick={handleRegisterClick} />} />
+    <Route path='/register' element={logic.isUserLoggedIn() ? <Navigate to='/' /> : <Register onRegister={handleRegister} onLoginClick={handleLoginClick} />} />
+    <Route path='/contact' element={<Contact onRegisterPetsitterClick={handleRegisterClick} />} />
+
   </Routes>
 }
