78027c66e7a5b36670215ed9198b7ef6ab195c1d
Tatiana_Garcia <151873298+TatiGV@users.noreply.github.com>
2024-08-15 19:35:45 +0200

    modified validation of loginUser's logic #99

--- staff/tatiana-garcia/project/app/logic/loginUser.js
+++ staff/tatiana-garcia/project/app/logic/loginUser.js
@@ -3,7 +3,7 @@ import { validate, errors } from '../../com/index.js'
 const { SystemError } = errors
 
 export default (username, password) => {
-    validate.username(username)
+    validate.username(username, 'username')
     validate.password(password)
 
     return fetch(`${import.meta.env.VITE_API_URL}/users/auth`, {
