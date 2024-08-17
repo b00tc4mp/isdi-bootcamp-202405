c941d2372debbcaf28ebc3afac0f543e65c42b18
Marti Herms <mhermsrubio@gmail.com>
2024-08-17 13:11:55 +0200

    implement edit avatar and username logic in app #84

--- staff/marti-herms/project/V-HUB/app/logic/index.js
+++ staff/marti-herms/project/V-HUB/app/logic/index.js
@@ -16,6 +16,8 @@ import makeReview from './makeReview.js'
 import deleteReview from './deleteReview.js'
 import getUserAvatar from './getUserAvatar.js'
 import getUser from './getUser.js'
+import editUserAvatar from './editUserAvatar.js'
+import editUserUsername from './editUserUsername.js'
 
 const logic = {
     loginUser,
@@ -35,7 +37,9 @@ const logic = {
     makeReview,
     deleteReview,
     getUserAvatar,
-    getUser
+    getUser,
+    editUserAvatar,
+    editUserUsername
 }
 
 export default logic
