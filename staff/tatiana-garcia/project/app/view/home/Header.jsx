7786a10574e197290f28630fafb4716768f85e7e
Tatiana Garcia <tatiana.garcia.vald@gmail.com>
2024-12-11 10:32:49 +0100

    Add when you click on the name go back to edit user#99

--- staff/tatiana-garcia/project/app/view/home/Header.jsx
+++ staff/tatiana-garcia/project/app/view/home/Header.jsx
@@ -9,12 +9,17 @@ import Link from '../library/Link.jsx';
 
 export default function Header() {
     const [name, setName] = useState(null);
+    const [role, setRole] = useState(null);
 
     useEffect(() => {
         if (logic.isUserLoggedIn()) {
             logic.getUserName()
                 .then(name => {
                     setName(name);
+                    return logic.getUserRole();
+                })
+                .then(role => {
+                    setRole(role);
                 })
                 .catch(error => {
                     console.error(error);
@@ -23,21 +28,26 @@ export default function Header() {
         }
     }, []);
 
-    return <header className='bg-stone-100 fixed left-0 top-0 w-full flex items-center gap-2 p-2 z-40'>
-        <Link className='flex-shrink' href={'/'}>
-            <img src='../../../logoExoticus.png' alt='logo exoticus' className='h-[3rem] w-auto' />
-        </Link>
-        <Container>
-            <Heading className=' text-2xl font-bold text-black italic m-1'>Exoticus</ Heading>
-        </Container>
-        <Container className='font-bold text-right flex flex-col text-sm ml-auto'>
-            {
-                name ? (
-                    <Paragraph>{name}</Paragraph>
+    return (
+        <header className='bg-stone-100 fixed left-0 top-0 w-full flex items-center gap-2 p-2 z-40'>
+            <Link className='flex-shrink' href={'/'}>
+                <img src='../../../logoExoticus.png' alt='logo exoticus' className='h-[3rem] w-auto' />
+            </Link>
+            <Container>
+                <Heading className='text-2xl font-bold text-black italic m-1'>Exoticus</Heading>
+            </Container>
+            <Container className='font-bold text-right flex flex-col text-sm ml-auto'>
+                {name ? (
+                    <Link
+                        href={role === 'petsitter' ? '/settings' : '/settings'}
+                        className='text-black underline hover:text-black'
+                    >
+                        <Paragraph>{name}</Paragraph>
+                    </Link>
                 ) : (
                     <Paragraph>Bienvenida/o!</Paragraph>
-                )
-            }
-        </Container>
-    </header>;
+                )}
+            </Container>
+        </header>
+    );
 }
