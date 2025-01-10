942a80dc3c05bf4d4286d82352e3ae86b69191bb
Tatiana Garcia <tatiana.garcia.vald@gmail.com>
2025-01-10 09:14:03 +0100

    Modified Alert styles #99

--- staff/tatiana-garcia/project/app/view/common/Alert.jsx
+++ staff/tatiana-garcia/project/app/view/common/Alert.jsx
@@ -1,17 +1,17 @@
-import Container from '../library/Container'
-import Button from '../library/Button'
-import Paragraph from '../library/Paragraph'
+import Container from '../library/Container';
+import Button from '../library/Button';
+import Paragraph from '../library/Paragraph';
 
 export default function Alert({ message, onAccept }) {
     return <>
         <Container className='fixed w-screen top-0 h-screen bg-black opacity-50'>
         </Container>
 
-        <Container className='fixed w-screen top-0 h-screen flex items-center justify-center'>
-            <Container className='p-4 border bg-white dark:bg-black dark:text-white flex-col'>
+        <Container className='fixed w-screen top-0 h-screen flex items-center text-center justify-center '>
+            <Container className='p-4 border-2 border-black rounded-[20px] bg-teal-200 text-black font-bold text-base flex-col space-y-2'>
                 <Paragraph>{message}</Paragraph>
-                <Button onClick={onAccept}>Accept</Button>
+                <Button className='border border-black bg-slate-200 w-24 rounded-md' onClick={onAccept}>Aceptar</Button>
             </Container>
         </Container>
-    </>
+    </>;
 }
