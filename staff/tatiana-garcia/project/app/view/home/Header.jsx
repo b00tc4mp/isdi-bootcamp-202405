c6fe30abbaf1710b00229fc3c392c1868a8b5691
Tatiana Garcia <tatiana.garcia.vald@gmail.com>
2025-01-15 14:30:12 +0100

    Put message icon in header #99

--- staff/tatiana-garcia/project/app/view/home/Header.jsx
+++ staff/tatiana-garcia/project/app/view/home/Header.jsx
@@ -6,6 +6,8 @@ import Container from '../library/Container.jsx';
 import Heading from '../library/Heading.jsx';
 import Paragraph from '../library/Paragraph.jsx';
 import Link from '../library/Link.jsx';
+import { IoChatboxEllipsesOutline } from 'react-icons/io5';
+import Button from '../library/Button.jsx';
 
 export default function Header() {
     const [name, setName] = useState(null);
@@ -36,14 +38,16 @@ export default function Header() {
             <Container>
                 <Heading className='text-2xl font-bold font-salsa text-black italic m-1'>Exoticus</Heading>
             </Container>
-            <Container className='font-bold text-right flex flex-col text-sm ml-auto'>
+            <Container className='font-bold font-salsa text-right flex space-x-2 text-sm ml-auto'>
                 {name ? (
-                    <Link
-                        href={role === 'petsitter' ? '/settings' : '/settings'}
-                        className='text-black underline hover:text-black'
-                    >
-                        <Paragraph>{name}</Paragraph>
-                    </Link>
+                    <>
+                        <Link
+                            href={role === 'petsitter' ? '/settings' : '/settings'}
+                        >
+                            <Paragraph>{name}</Paragraph>
+                        </Link>
+                        <Button><IoChatboxEllipsesOutline size={22} /></Button>
+                    </>
                 ) : (
                     <Paragraph>Bienvenida/o!</Paragraph>
                 )}
